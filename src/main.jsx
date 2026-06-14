bc9d06ff6959b94fd4441689809d06b6357dc59f
Mile <thatoneextramile@gmail.com>
2026-06-14 16:26:16 -0700

    change t1 - t6 to 1t - 6t message presentation update

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "../WonderworldApp_1.jsx";
+import App from "../WonderworldApp_2.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
